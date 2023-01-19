336e881231edf19665e9269b17f0e819c3c8b924
NartovEvgenii <evgeniynartov36rus@gmail.com>
2023-01-19 20:59:36 +0300

    init

---
+++ go/enemy_spawner.go
@@ -0,0 +1,51 @@
+components {
+  id: "enemy_spawner"
+  component: "/scripts/enemy_spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory_large"
+  type: "factory"
+  data: "prototype: \"/go/enemy.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory_small"
+  type: "factory"
+  data: "prototype: \"/go/enemy_small.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ go/spawner.go
@@ -0,0 +1,71 @@
+components {
+  id: "explosion_spawner"
+  component: "/scripts/explosion_spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "scale"
+    value: "0.3"
+    type: PROPERTY_TYPE_NUMBER
+  }
+}
+components {
+  id: "ship_spawner"
+  component: "/scripts/ship_spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/go/explosion.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "ship_factory"
+  type: "collectionfactory"
+  data: "prototype: \"/go/ship.collection\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
